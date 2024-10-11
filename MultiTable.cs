0a851ecfd35a2f813f85d88ce90e20d88d3914a9
Ivan Rogozhin <vanad3411@mail.ru>
2024-10-11 17:11:31 +0300

    Добавьте файлы проекта.

---
+++ MultiTable.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace SlonitTEST
+{
+    class MultiTable (int multiplier)
+    {
+        private int maxSymb = (multiplier * multiplier).ToString().Length;
+        private string FillSpaces(string inputString)
+        {
+            //while (inputString.Length < maxSymb + 1)
+            //{
+            //    inputString = " " + inputString;
+            //}
+            inputString = inputString.PadLeft(maxSymb+1);
+            return inputString;           
+        }
+
+        public void ShowInConsole()
+        {
+            Console.WriteLine();
+            for (int i = 0; i != multiplier + 1; i++)
+            {
+                if (i == 0)
+                {
+                    Console.Write(FillSpaces(""));
+                    for (int j = 1; j != multiplier + 1; j++)
+                    {
+                        Console.Write(FillSpaces(j.ToString()));
+                    }
+                    Console.WriteLine();
+                }
+                else
+                {
+                    Console.Write(FillSpaces(i.ToString()));
+                    for (int j = 1; j != multiplier + 1; j++)
+                    {
+                        Console.Write(FillSpaces((j * i).ToString()));
+                    }
+                    Console.WriteLine();
+                }
+            }
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+//
+//
+//
+// 4 Таблица умножения - 45 минут
+namespace SlonitTEST
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            MultiTable mTable = new MultiTable(45);
+            mTable.ShowInConsole();
+        }
+    }
+}
