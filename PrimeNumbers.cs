17992c8d4a896600e0a674254c812943b2067970
Ivan Rogozhin <vanad3411@mail.ru>
2024-10-11 18:21:35 +0300

    Added ManyComputers, PrimeNumbers

---
+++ PrimeNumbers.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SlonitTEST
+{
+    internal class PrimeNumbers
+    {
+        private int startN, endN;
+        PrimeNumbers(int startN, int endN)
+        {
+            this.startN = startN;
+            this.endN = endN;
+        }   
+        
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,7 +1,7 @@
 ﻿using System;
-//
-//
-//
+// 1 Много комьютера - 27 минут
+// 2 Общий делитель массива чисел
+// 3 Простые числа в диапазоне
 // 4 Таблица умножения - 45 минут
 namespace SlonitTEST
 {
@@ -10,8 +10,25 @@ namespace SlonitTEST
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            //-------------------------4
             MultiTable mTable = new MultiTable(45);
-            mTable.ShowInConsole();
+            //-------------------------1
+            int temp;
+            while (true)
+            {
+                string stemp = Console.ReadLine();
+                try
+                {
+                    temp = Int32.Parse(stemp);
+                    Console.WriteLine(ManyComputers.HowMany(temp));
+                }
+                catch (Exception)
+                {
+                    temp = 0;
+                    Console.WriteLine("Ошибка, попробуйте еще");
+                } 
+                
+            }
         }
     }
 }
